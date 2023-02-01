790ae1c340453e8dba9fd6426e787afa34f4e4ee
sebastian <ssp013sanchez@gmail.com>
2023-02-01 16:03:36 -0300

    isadmin

--- src/App.js
+++ src/App.js
@@ -25,22 +25,77 @@ function App() {
       <ConfigProvider locale={lang}>
         <Network />
         <Switch>
-          {
-              token ?
-              isAdmin ? (
-              <Layout>
-                {/* <Route exact path="/" component={Login} /> */}
-                <Route path="/users" component={Users} />
-              </Layout>
-              ) : 
-              <Layout>
-                {/* <Route exact path="/" component={Login} /> */}
-                <Route path="/form" component={Form} />
-                <Route path="/forms" component={Forms} />
-              </Layout>
-            :   null
+         {token ? <Layout>
+         {
+          !isAdmin ? 
+            <>
+            <Route path="/users" component={Users} />
+            <Redirect to="/" />
+            </> : 
+            <>
+            <Route exact path="/" component={Login} />
+            <Route path="/form" component={Form} />
+            <Route path="/forms" component={Forms} />
+            <Redirect to="/" />
+            </>
+            
+         }
+           
+          </Layout> : null 
           }
-          <Route exact path="/" component={<Login/>} />
+          <Route exact path="/" component={Login} />
+          <Redirect to="/" />
+        </Switch>
+      </ConfigProvider>
+    </ThemeProvider>
+  );
+}
+/* 
+  {
+            isAdmin? <>
+              <Route path="/users" component={Users} />
+            </> :
+            <>
+              <Route exact path="/" component={Login} />
+              <Route path="/form" component={Form} />
+              <Route path="/forms" component={Forms} />
+            </>
+
+          }
+
+*/
+
+
+
+export default App;
+/* import React from 'react';
+import { ConfigProvider } from 'antd';
+import Network from 'ui/Network/Network';
+import { Switch, Redirect, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { useTheme } from 'hooks/themeHook';
+import { useLang } from 'hooks/langHook';
+import Login from 'pages/Login';
+import Form from 'pages/Form/Form';
+import Forms from 'pages/Forms/Forms';
+import Layout from 'components/Layout/Layout';
+import useAccountStore from 'store/common/account';
+function App() {
+  const [theme] = useTheme();
+  const [lang] = useLang();
+  const {token} = useAccountStore();
+  return (
+    <ThemeProvider theme={theme}>
+      <ConfigProvider locale={lang}>
+        <Network />
+        <Switch>
+        
+          {token ? <Layout>
+            <Route exact path="/" component={Login} />
+            <Route path="/form" component={Form} />
+            <Route path="/forms" component={Forms} />
+          </Layout> : null }
+          <Route exact path="/" component={Login} />
           <Redirect to="/" />
         </Switch>
       </ConfigProvider>
@@ -48,4 +103,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; */
