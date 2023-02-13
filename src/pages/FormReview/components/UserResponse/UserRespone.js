5a21c6e4d243dbb36ba2aff0ace7d771964a816f
Dionisio Olivares <dionisio.olivares23@gmail.com>
2023-02-13 11:50:38 -0300

    add excel responses

--- src/pages/FormReview/components/UserResponse/UserRespone.js
+++ src/pages/FormReview/components/UserResponse/UserRespone.js
@@ -6,14 +6,18 @@ import React, { useEffect,useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import useStoreFormUserResponse from './store';
 import Layout from 'components/Layout/Layout';
-import { getDataModel } from './service';
-import { Badge, Button, Divider, Empty, InputNumber, List } from 'antd';
+import { getDataModel, getDataReport } from './service';
+import { Badge, Button, Divider, Empty, InputNumber, List, Menu, Progress } from 'antd';
 import Title from 'ui/Title/Title';
+import { green, red } from '@ant-design/colors';
+import ExcelExport from 'components/Excel/ExcelExport/ExcelExport';
+import MoreOptionsButton from 'components/MoreOptionsButton/MoreOptionsButton';
 
 
 const UserResponse = (props) => {
   const {t} = useTranslation();
   const [dataModel, setDataModel] = useState(null)
+  const [dataReport, setDataReport] = useState(null)
   
   const {requestUserResponse, userResponseValue, loading} = useStoreFormUserResponse();
 
@@ -26,6 +30,11 @@ const UserResponse = (props) => {
     const data = getDataModel({data: userResponseValue},{t})
     setDataModel(data)
   }, [userResponseValue]);
+  
+  useEffect(() => {
+    const data = getDataReport({data: dataModel},{t})
+    setDataReport(data)
+  }, [dataModel]);
 
   const SectionDetail = (props) => {
     const {name, duration, children} = props
@@ -66,13 +75,30 @@ const UserResponse = (props) => {
     </>
   }
 
+  const ExportMenu = () => {
+    return (
+    <MoreOptionsButton
+        disabled={false}
+        menu={
+            <Menu style={{width: 'auto'}}>
+                <ExcelExport
+                    key="1"
+                    data={dataReport}
+                    sheetname={'file'}
+                    filename={`respuestas_formulario`}
+                    isMenuItem={true}
+                />
+            </Menu>
+        }
+    />);
+}
 
 
 
   return (
     <StyledFormReview>
       <Layout.Content>
-        <Section  title={'Respuestas del usuario'} icon={<AlertOutlined />} loading={loading} shadow>
+        <Section  title={'Respuestas del usuario'} icon={<AlertOutlined />} loading={loading} shadow tools={<ExportMenu/>}>
             {
               dataModel ?
                 dataModel?.map((section)=> {
@@ -88,12 +114,18 @@ const UserResponse = (props) => {
                                 /* avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />} */
                                 title={item.question}
                                 description={
-                                  <div style={{display: "flex"}}>
+                                  <div style={{display: "flex", alignItems: "center"}}>
+                                    {
+                                      item.is_linear_question? <div  style={{marginRight: 15, display: "grid"}}>
+                                      <Progress percent={item.linear_question_answer_scale} steps={5} strokeWidth={12} showInfo={false} strokeColor={[green[6], green[6], red[5]]}/>
+                                      </div> : <></>
+                                    }
                                     {item.answer}
                                     <div style={{marginLeft: 15}}>
                                       {item.is_correct == true? <CheckCircleOutlined style={{color: "green", fontSize: 20}}/> : <></>}
                                       {item.is_correct == false? <CloseCircleOutlined style={{color: "red", fontSize: 20}}/> : <></>}
                                     </div>
+                                   
                                   </div>
                                 }
                                 />
--- src/pages/FormReview/components/UserResponse/service.js
+++ src/pages/FormReview/components/UserResponse/service.js
@@ -1,6 +1,5 @@
 export const getDataModel = ({data},{t}) => {
     const dataSource = []
-
     const sectionsID = []
     const sections = []
 
@@ -15,6 +14,7 @@ export const getDataModel = ({data},{t}) => {
             })
         }
     })
+
     sections?.forEach((item) => {
         data?.time_per_section?.forEach((item_time) => {
             if(item.id == item_time.section_id){
@@ -22,9 +22,6 @@ export const getDataModel = ({data},{t}) => {
             }
         })
     })
-
-    
-   
 
     sections.forEach((section) => {
         const dataResponse = []
@@ -34,6 +31,8 @@ export const getDataModel = ({data},{t}) => {
                     question: item.question,
                     answer: item.answer,
                     is_open_question: item.is_open_question,
+                    is_linear_question: item.AnswerInt > 0 ? true : false,
+                    linear_question_answer_scale: item.AnswerInt > 0 ? 20 * item.AnswerInt : undefined,
                     question_id: item.question_id,
                     answer_id: {
                         answers_selection_id: item?.answers_selection_id,
@@ -48,7 +47,6 @@ export const getDataModel = ({data},{t}) => {
                 dataResponse.push(dataResp)
             }  
         })
-
         dataResponse && dataSource.push(
             {
                 section_id: section.id,
@@ -58,12 +56,87 @@ export const getDataModel = ({data},{t}) => {
                 responses: dataResponse
             })
     })
-    
-  
     console.log(dataSource)
     return dataSource;
-  }
-
-const uniqueSection = (value, index, self) => {
-    return self.indexOf(value) === index;
 }
+
+export const getDataReport = (
+    { data },
+    { t, border, color }
+  ) => {
+    if (data) {
+  
+      const titleStyle = {
+        font: {
+          bold: true
+        },
+        fill: {
+          patternType: "solid",
+          fgColor: {
+            rgb: "FFCCEEFF"
+          }
+        }
+      }
+  
+      const columns = [
+        {
+          title: "Sección",
+          width: {
+            wpx: 200
+          },
+          style: titleStyle
+        },
+        {
+          title: "Pregunta",
+          width: {
+            wpx: 200
+          },
+          style: titleStyle
+        },
+        {
+          title: "Respuesta",
+          width: {
+            wpx: 200
+          },
+          style: titleStyle
+        },        
+      ]
+  
+      const data2 = []
+      data?.forEach((section) => {
+        section?.responses?.forEach((response) => {
+            const responsesData = [
+                {
+                    value: section.section_name,
+                },    
+                {
+                    value: response?.question,
+                },
+                {
+                    value: response?.answer,
+                },    
+            ]
+            data2.push(responsesData)
+        })
+      })
+      const dataUsers = [
+                [
+                    {value: "H1", style: {font: {sz: "24", bold: true}}},
+                    {value: "Bold", style: {font: {bold: true}}},
+                    {value: "Red", style: {fill: {patternType: "solid", fgColor: {rgb: "FFFF0000"}}}},
+                ],
+                [
+                    {value: "H2", style: {font: {sz: "18", bold: true}}},
+                    {value: "underline", style: {font: {underline: true}}},
+                    {value: "Blue", style: {fill: {patternType: "solid", fgColor: {rgb: "FF0000FF"}}}},
+                ],
+            ]
+  
+    const dataset = [{
+      columns: columns,
+      data: data2,
+    }]
+      console.log("data set: ",dataset);
+      return dataset;
+    }
+  };
